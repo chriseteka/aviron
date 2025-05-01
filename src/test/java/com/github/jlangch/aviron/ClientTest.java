75cbbf055a02a10b18064bcd1b34720639f970c1
jlangch <juerg.lang@ggaweb.ch>
2025-05-01 11:55:23 +0200

    updated doc

--- src/test/java/com/github/jlangch/aviron/ClientTest.java
+++ src/test/java/com/github/jlangch/aviron/ClientTest.java
@@ -2,8 +2,10 @@ package com.github.jlangch.aviron;
 
 import org.junit.jupiter.api.Test;
 
+
 class ClientTest {
-	
+
     @Test void test() {
+        // without clamd mock the client must be mainly manually tested
     }
 }
