548d64cb4b702aaa28b4fae9e35acf7586cce949
jlangch <juerg.lang@ggaweb.ch>
2025-05-04 12:40:06 +0200

    added license info

--- src/main/java/com/github/jlangch/aviron/Admin.java
+++ src/main/java/com/github/jlangch/aviron/Admin.java
@@ -1,3 +1,25 @@
+/*                 _                 
+ *       /\       (_)            
+ *      /  \__   ___ _ __ ___  _ __  
+ *     / /\ \ \ / / | '__/ _ \| '_ \ 
+ *    / ____ \ V /| | | | (_) | | | |
+ *   /_/    \_\_/ |_|_|  \___/|_| |_|
+ *
+ *
+ * Copyright 2025 Aviron
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.jlangch.aviron;
 
 import java.io.IOException;
@@ -10,6 +32,7 @@ import com.github.jlangch.aviron.util.OS;
 import com.github.jlangch.aviron.util.Shell;
 import com.github.jlangch.aviron.util.ShellResult;
 import com.github.jlangch.aviron.util.StringUtils;
+import com.github.jlangch.aviron.util.Version;
 
 
 /**
@@ -25,6 +48,13 @@ import com.github.jlangch.aviron.util.StringUtils;
  * </pre>
  */
 public class Admin {
+
+    /**
+     * @return the Aviron version
+     */
+    public static String getAvironVersion() {
+        return Version.VERSION;
+    }
 
     /**
      * Returns the <i>clamd</i> PID or <code>null</code> if <i>clamd</i> daemon 
