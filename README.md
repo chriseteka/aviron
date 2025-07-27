6661800bdef0236bfc44cb6e6c3f758d9a2616f1
jlangch <juerg.lang@ggaweb.ch>
2025-07-27 17:13:57 +0200

    added readme example

--- README.md
+++ README.md
@@ -432,7 +432,123 @@ Time        Mon    Tue    Wed    Thu    Fri    Sat    Sun
 
 ## Controlling the Clamd CPU usage
 
-*documentation work in progress...*
+Demonstrates how a demo filestore can be continously scanned honoring a *clamd*
+CPU profile.
+
+
+Our demo filestore looks like:
+
+```
+/data/filestore/
+  |
+  +-- 0000
+  |     \_ file1.doc
+  |     \_ file2.doc
+  |     :
+  |     \_ fileN.doc
+  +-- 0001
+  :
+  +-- NNNN
+        \_ file1.doc
+```
+
+and the demo code:
+
+```java
+import static com.github.jlangch.aviron.impl.util.CollectionUtils.toList;
+
+import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.github.jlangch.aviron.Client;
+import com.github.jlangch.aviron.FileSeparator;
+import com.github.jlangch.aviron.admin.ClamdAdmin;
+import com.github.jlangch.aviron.admin.ClamdCpuLimiter;
+import com.github.jlangch.aviron.admin.CpuProfile;
+import com.github.jlangch.aviron.admin.DynamicCpuLimit;
+import com.github.jlangch.aviron.events.QuarantineEvent;
+import com.github.jlangch.aviron.events.QuarantineFileAction;
+
+
+public class ClamdCpuLimiterExample {
+
+    public static void main(String[] args) {
+        try {
+            new ClamdCpuLimiterExample().scan();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void scan() throws Exception {
+        final File filestoreDir = new File("/data/filestore/");
+        final File quarantineDir = new File("/data/quarantine/");
+
+        final Client client = new Client.Builder()
+                                        .serverHostname("localhost")
+                                        .serverFileSeparator(FileSeparator.UNIX)
+                                        .quarantineFileAction(QuarantineFileAction.MOVE)
+                                        .quarantineDir(quarantineDir)
+                                        .quarantineEventListener(this::eventListener)
+                                        .build();
+
+        // Use the same day profile for Mon - Sun
+        final CpuProfile everyday = CpuProfile.of(
+                                        "weekday",
+                                        toList(
+                                            "00:00-05:59 @ 100%",
+                                            "06:00-08:59 @  50%",
+                                            "09:00-17:59 @   0%",
+                                            "18:00-21:59 @  50%",
+                                            "22:00-23:59 @ 100%"));
+
+        final String clamdPID = ClamdAdmin.getClamdPID();
+
+        final ClamdCpuLimiter limiter = new ClamdCpuLimiter(new DynamicCpuLimit(everyday));
+
+        // inital CPU limit after startup
+        limiter.activateClamdCpuLimit(clamdPID);
+        System.out.println(String.format(
+                            "Initial clamd CPU limit: %d%%",
+                            limiter.getLastSeenLimit()));
+
+        // scan until we're killed
+        while(true) {
+            try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(filestoreDir.toPath())) {
+                dirStream.forEach(path -> {
+                    // update clamd CPU limit 
+                    // note: applied only if the new limit differs from the last one
+                    final int lastSeenLimit = limiter.getLastSeenLimit();
+                    if (limiter.activateClamdCpuLimit(clamdPID)) {
+                        final int newLimit = limiter.getLastSeenLimit();
+                        System.out.println(String.format(
+                                            "Adjusted clamd CPU limit: %d%% -> %d%%",
+                                            lastSeenLimit, newLimit));
+                    }
+
+                    // Scan the next filestore directory
+                    System.out.println(client.scan(path, false));
+                });
+            }
+            catch(Exception ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        }
+    }
+
+    private void eventListener(final QuarantineEvent event) {
+        if (event.getException() != null) {
+            System.out.println("Error " + event.getException().getMessage());
+        }
+        else {
+            System.out.println("File " + event.getInfectedFile() + " moved to quarantine");
+        }
+    }
+}
+```
 
 
 ## Getting the latest release
--- src/main/java/com/github/jlangch/aviron/examples/ClamdCpuLimiterExample.java
+++ src/main/java/com/github/jlangch/aviron/examples/ClamdCpuLimiterExample.java
@@ -24,22 +24,58 @@ package com.github.jlangch.aviron.examples;
 
 import static com.github.jlangch.aviron.impl.util.CollectionUtils.toList;
 
+import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.github.jlangch.aviron.Client;
+import com.github.jlangch.aviron.FileSeparator;
+import com.github.jlangch.aviron.admin.ClamdAdmin;
+import com.github.jlangch.aviron.admin.ClamdCpuLimiter;
 import com.github.jlangch.aviron.admin.CpuProfile;
 import com.github.jlangch.aviron.admin.DynamicCpuLimit;
+import com.github.jlangch.aviron.events.QuarantineEvent;
+import com.github.jlangch.aviron.events.QuarantineFileAction;
 
 
 public class ClamdCpuLimiterExample {
 
     public static void main(String[] args) {
         try {
-            new ClamdCpuLimiterExample().test();
+            new ClamdCpuLimiterExample().scan();
         }
         catch(Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public void test() throws Exception {
+    public void scan() throws Exception {
+        // Our demo filestore looks like:
+        //
+        // /data/filestore/
+        //   |
+        //   +-- 0000
+        //   |     \_ file1.doc
+        //   |     \_ file2.doc
+        //   |     :
+        //   |     \_ fileN.doc
+        //   +-- 0001
+        //   :
+        //   +-- NNNN
+        //         \_ file1.doc
+        //
+        final File filestoreDir = new File("/data/filestore/");
+        final File quarantineDir = new File("/data/quarantine/");
+
+        final Client client = new Client.Builder()
+                                        .serverHostname("localhost")
+                                        .serverFileSeparator(FileSeparator.UNIX)
+                                        .quarantineFileAction(QuarantineFileAction.MOVE)
+                                        .quarantineDir(quarantineDir)
+                                        .quarantineEventListener(this::eventListener)
+                                        .build();
+
         // Use the same day profile for Mon - Sun
         final CpuProfile everyday = CpuProfile.of(
                                         "weekday",
@@ -50,12 +86,47 @@ public class ClamdCpuLimiterExample {
                                             "18:00-21:59 @  50%",
                                             "22:00-23:59 @ 100%"));
 
-        final DynamicCpuLimit dynamicCpuLimit = new DynamicCpuLimit(everyday);
+        final String clamdPID = ClamdAdmin.getClamdPID();
 
-        // Even though the profile has a minute resolution the 
-        // 'formatProfilesAsTableByHour' function prints the overview
-        //  table at an hour resolution for simplicity!
-        final String s = dynamicCpuLimit.formatProfilesAsTableByHour();
-        System.out.println(s);
+        final ClamdCpuLimiter limiter = new ClamdCpuLimiter(new DynamicCpuLimit(everyday));
+
+        // inital CPU limit after startup
+        limiter.activateClamdCpuLimit(clamdPID);
+        System.out.println(String.format(
+                            "Initial clamd CPU limit: %d%%",
+                            limiter.getLastSeenLimit()));
+
+        // scan until we're killed
+        while(true) {
+            try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(filestoreDir.toPath())) {
+                dirStream.forEach(path -> {
+                    // update clamd CPU limit 
+                    // note: applied only if the new limit differs from the last one
+                    final int lastSeenLimit = limiter.getLastSeenLimit();
+                    if (limiter.activateClamdCpuLimit(clamdPID)) {
+                        final int newLimit = limiter.getLastSeenLimit();
+                        System.out.println(String.format(
+                                            "Adjusted clamd CPU limit: %d%% -> %d%%",
+                                            lastSeenLimit, newLimit));
+                    }
+
+                    // Scan the next filestore directory
+                    System.out.println(client.scan(path, false));
+                });
+            }
+            catch(Exception ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        }
+    }
+
+
+    private void eventListener(final QuarantineEvent event) {
+        if (event.getException() != null) {
+            System.out.println("Error " + event.getException().getMessage());
+        }
+        else {
+            System.out.println("File " + event.getInfectedFile() + " moved to quarantine");
+        }
     }
 }
