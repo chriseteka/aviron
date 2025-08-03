a64f5099abf2929dc28235c8b52a518b39ebeee2
jlangch <juerg.lang@ggaweb.ch>
2025-08-03 15:31:58 +0200

    clearly incubation package for classes that may be released soon

--- src/main/java/com/github/jlangch/aviron/impl/incubation/RealtimeScanner.java
+++ src/main/java/com/github/jlangch/aviron/impl/incubation/RealtimeScanner.java
@@ -20,7 +20,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.jlangch.aviron.impl.filewatcher;
+package com.github.jlangch.aviron.impl.incubation;
 
 import java.io.File;
 import java.io.IOException;
