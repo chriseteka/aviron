e1a738396cc9aad407703f25550f4aeb6efbe9a3
jlangch <juerg.lang@ggaweb.ch>
2025-08-04 08:35:38 +0200

    added javadoc

--- src/main/java/com/github/jlangch/aviron/util/DemoUtil.java
+++ src/main/java/com/github/jlangch/aviron/util/DemoUtil.java
@@ -25,19 +25,68 @@ package com.github.jlangch.aviron.util;
 
 public class DemoUtil {
 
-    public static void sleep(final int millis) {
+    /**
+     * Causes the currently executing thread to sleep (temporarily cease
+     * execution) for the specified number of milliseconds, subject to
+     * the precision and accuracy of system timers and schedulers. The thread
+     * does not lose ownership of any monitors.
+     *
+     * @param  millis
+     *         the length of time to sleep in milliseconds
+     *
+     * @throws  IllegalArgumentException
+     *          if the value of {@code millis} is negative
+     */
+     public static void sleep(final int millis) {
         try { 
             Thread.sleep(millis);
         } 
         catch(Exception ex) {}
     }
 
+    /**
+     * A convenience method to print formatted strings.
+     * 
+     * @param  format
+     *         A format string as described in <a
+     *         href="../util/Formatter.html#syntax">Format string syntax</a>
+     *
+     * @param  args
+     *         Arguments referenced by the format specifiers in the format
+     *         string.  If there are more arguments than format specifiers, the
+     *         extra arguments are ignored.  The number of arguments is
+     *         variable and may be zero.  The maximum number of arguments is
+     *         limited by the maximum dimension of a Java array as defined by
+     *         <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *         The behaviour on a
+     *         <tt>null</tt> argument depends on the <a
+     *         href="../util/Formatter.html#syntax">conversion</a>.
+     */
     public static void printf(final String format, final Object... args) {
         synchronized(lock) {
             System.out.printf(format, args);
         }
     }
 
+    /**
+     * A convenience method to print formatted strings. Terminates the line by 
+     * writing the line separator string.
+     * 
+     * @param  format
+     *         A format string as described in <a
+     *         href="../util/Formatter.html#syntax">Format string syntax</a>
+     *
+     * @param  args
+     *         Arguments referenced by the format specifiers in the format
+     *         string.  If there are more arguments than format specifiers, the
+     *         extra arguments are ignored.  The number of arguments is
+     *         variable and may be zero.  The maximum number of arguments is
+     *         limited by the maximum dimension of a Java array as defined by
+     *         <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *         The behaviour on a
+     *         <tt>null</tt> argument depends on the <a
+     *         href="../util/Formatter.html#syntax">conversion</a>.
+     */
     public static void printfln(final String format, final Object... args) {
         synchronized(lock) {
             System.out.printf(format, args);
